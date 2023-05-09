ef54338f0480d28ff19f33eaacd03a0fd13c3e62
JoshuaRabbit <32034557+LUOHONGPENG@users.noreply.github.com>
2023-05-09 22:12:00 +0100

    BasicCookware

--- Assets/Scripts/Game/Cookware/CookwareBasic.cs
+++ Assets/Scripts/Game/Cookware/CookwareBasic.cs
@@ -4,5 +4,12 @@ using UnityEngine;
 
 public class CookwareBasic : MonoBehaviour
 {
+    [Header("BasicInfo")]
+    public CookwareType cookType;
+    public int cookID;
 
+    public void Init(int ID)
+    {
+
+    }
 }
