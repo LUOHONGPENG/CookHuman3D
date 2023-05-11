43df96c825183afdd656b0307539bb7c129548ec
JoshuaRabbit <32034557+LUOHONGPENG@users.noreply.github.com>
2023-05-11 21:31:47 +0100

    HumanUIBasic

--- Assets/Scripts/Game/Human/HumanBasicUIExt.cs
+++ Assets/Scripts/Game/Human/HumanBasicUIExt.cs
@@ -1,10 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public partial class HumanBasic
 {
-    public Transform tfAge;
+    [Header("HumanUI")]
+    public Transform tfAgeUI;
+    public Text txAge;
 
+    private void Update()
+    {
+        RefreshHumanUI();
+    }
 
+    public void RefreshHumanUI()
+    {
+        if (!isInit)
+        {
+            return;
+        }
+        //UI Position
+        tfAgeUI.localPosition = PublicTool.CalculateUICanvasPos(this.transform.position, GameMgr.Instance.mapCamera) + new Vector3(0, 100f, 0);
+        txAge.text = Mathf.FloorToInt(humanItem.Age).ToString();
+    }
 }
