01e07ac9bbb95dd94fdd4e7372292a45d1f731bd
JoshuaRabbit <32034557+LUOHONGPENG@users.noreply.github.com>
2023-05-26 13:40:10 +0100

    UpdatePopup

--- Assets/Scripts/Game/UI/Retire/RetireMiniUIMgr.cs
+++ Assets/Scripts/Game/UI/Retire/RetireMiniUIMgr.cs
@@ -1,13 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class RetireMiniUIMgr : MonoBehaviour
 {
     public GameObject objBlock;
+    public RectTransform rtInfo;
+    public Button btnClose;
 
+    [Header("Score")]
+    public Text codeScore;
 
+    [Header("Comment")]
+    public Transform tfComment;
+    public GameObject pfComment;
+
+    //Inner Data
+    private float timerClose = 5f;
     private bool isInit = false;
+
     public void Init()
     {
         objBlock.SetActive(false);
@@ -15,5 +28,17 @@ public class RetireMiniUIMgr : MonoBehaviour
         isInit = true;
     }
 
+    public void ShowPopup()
+    {
+        timerClose = 5f;
+    }
 
+
+    private void Update()
+    {
+        if (timerClose > 0)
+        {
+            timerClose -= Time.deltaTime;
+        }
+    }
 }
